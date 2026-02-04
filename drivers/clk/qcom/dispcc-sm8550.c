7e7832bc6fef26ec6011965d26dcf7c845ab14d2
Jagadeesh Kona <jagadeesh.kona@oss.qualcomm.com>
2026-02-03 18:00:46 -0600

    clk: qcom: sm8550: Don't hold state on some graphics related clocks
    Change-Id: If787b1ec02ff721682662d2833ac67d48ba706fa

--- drivers/clk/qcom/dispcc-sm8550.c
+++ drivers/clk/qcom/dispcc-sm8550.c
@@ -825,7 +825,7 @@ static struct clk_branch disp_cc_mdss_ahb1_clk = {
 				&disp_cc_mdss_ahb_clk_src.clkr.hw,
 			},
 			.num_parents = 1,
-			.flags = CLK_SET_RATE_PARENT,
+			.flags = CLK_SET_RATE_PARENT | CLK_DONT_HOLD_STATE,
 			.ops = &clk_branch2_ops,
 		},
 	},
