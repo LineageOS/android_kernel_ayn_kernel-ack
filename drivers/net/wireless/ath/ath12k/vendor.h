9d9b6f4b90c2e6cd44462b08102b3b091f9c1828
Aaron Kling <webgeek1234@gmail.com>
2026-04-03 18:13:37 -0500

    wifi: ath12k: Add get device info vendor command
    Change-Id: I2351257b508481089fa808201ced7b7339b19746

---
+++ drivers/net/wireless/ath/ath12k/vendor.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+/*
+ * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef ATH12K_VENDOR_H
+#define ATH12K_VENDOR_H
+
+int ath12k_vendor_register(struct ieee80211_hw *hw);
+
+#endif /* ATH12K_VENDOR_H */
+
