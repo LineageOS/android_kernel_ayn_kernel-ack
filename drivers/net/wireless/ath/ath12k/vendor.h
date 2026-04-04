815e2c4d456916e2f948d3e9febb08467b07eb0b
Aaron Kling <webgeek1234@gmail.com>
2026-04-03 23:41:58 -0500

    wifi: ath12k: Add supported features vendor command
    Change-Id: Ic8f58f3ab1ee3be8a0c92dd4dc2a1f2211e8f7b7

--- drivers/net/wireless/ath/ath12k/vendor.h
+++ drivers/net/wireless/ath/ath12k/vendor.h
@@ -6,6 +6,37 @@
 #ifndef ATH12K_VENDOR_H
 #define ATH12K_VENDOR_H
 
+/* Feature defines */
+#define WIFI_FEATURE_INFRA              0x0001  /* Basic infrastructure mode */
+#define WIFI_FEATURE_INFRA_5G           0x0002  /* Support for 5 GHz Band */
+#define WIFI_FEATURE_HOTSPOT            0x0004  /* Support for GAS/ANQP */
+#define WIFI_FEATURE_P2P                0x0008  /* Wifi-Direct */
+#define WIFI_FEATURE_SOFT_AP            0x0010  /* Soft AP */
+#define WIFI_FEATURE_EXTSCAN            0x0020  /* Extended Scan APIs */
+#define WIFI_FEATURE_NAN                0x0040  /* Neighbor Awareness Networking */
+#define WIFI_FEATURE_D2D_RTT            0x0080  /* Device-to-device RTT */
+#define WIFI_FEATURE_D2AP_RTT           0x0100  /* Device-to-AP RTT */
+#define WIFI_FEATURE_BATCH_SCAN         0x0200  /* Batched Scan (legacy) */
+#define WIFI_FEATURE_PNO                0x0400  /* Preferred network offload */
+#define WIFI_FEATURE_ADDITIONAL_STA     0x0800  /* Support for two STAs */
+#define WIFI_FEATURE_TDLS               0x1000  /* Tunnel directed link setup */
+#define WIFI_FEATURE_TDLS_OFFCHANNEL    0x2000  /* Support for TDLS off channel */
+#define WIFI_FEATURE_EPR                0x4000  /* Enhanced power reporting */
+#define WIFI_FEATURE_AP_STA             0x8000  /* Support for AP STA Concurrency */
+#define WIFI_FEATURE_LINK_LAYER_STATS   0x10000  /* Link layer stats */
+#define WIFI_FEATURE_LOGGER             0x20000  /* WiFi Logger */
+#define WIFI_FEATURE_HAL_EPNO           0x40000  /* WiFi PNO enhanced */
+#define WIFI_FEATURE_RSSI_MONITOR       0x80000  /* RSSI Monitor */
+#define WIFI_FEATURE_MKEEP_ALIVE        0x100000  /* WiFi mkeep_alive */
+#define WIFI_FEATURE_CONFIG_NDO         0x200000  /* ND offload configure */
+#define WIFI_FEATURE_TX_TRANSMIT_POWER  0x400000  /* Tx transmit power levels */
+#define WIFI_FEATURE_CONTROL_ROAMING    0x800000  /* Enable/Disable roaming */
+#define WIFI_FEATURE_IE_ALLOWLIST       0x1000000 /* Support Probe IE allow listing */
+#define WIFI_FEATURE_SCAN_RAND          0x2000000 /* Support MAC & Probe Sequence Number randomization */
+#define WIFI_FEATURE_SET_TX_POWER_LIMIT 0x4000000 /* Support Tx Power Limit setting */
+#define WIFI_FEATURE_DYNAMIC_SET_MAC    0x10000000 /* Support changing MAC address without iface reset(down and up) */
+#define WIFI_FEATURE_SET_LATENCY_MODE   0x40000000 /* Set latency mode */
+
 int ath12k_vendor_register(struct ieee80211_hw *hw);
 
 #endif /* ATH12K_VENDOR_H */
